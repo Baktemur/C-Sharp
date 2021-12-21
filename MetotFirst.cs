5916218432dde79b59b8a81cb18b0201cd82bdff
Baktemur <75330735+Baktemur@users.noreply.github.com>
2021-12-21 20:21:58 +0300

    Add files via upload

---
+++ MetotFirst.cs
@@ -0,0 +1,90 @@
+﻿
+//  PARAMETRELİ METOTLAR
+
+
+
+
+//    Add();
+//    Add();
+//    Add();
+//    Add();
+//    var result = Add2(15, 20); // return ün döndürmesi için bu class da tanımlamamız lazım
+
+//Console.WriteLine(result);
+//Console.ReadLine();
+//static void Add()
+//{
+
+
+
+//    Console.WriteLine("Added!!!");
+
+//}
+
+//static int Add2(int number1, int number2)
+//{
+
+//    return number1 + number2;
+
+//    //var result = number1 + number2;
+//    // return result; // üstteki kodun alternatifidir
+
+//}
+
+
+
+// DEFAULT PARAMETRELERLE ÇALIŞMAK
+
+//var result = Add2(); // return ün döndürmesi için bu class da tanımlamamız lazım
+
+//Console.WriteLine(result);
+//Console.ReadLine();
+//static void Add()
+//{
+
+
+
+//    Console.WriteLine("Added!!!");
+
+//}
+//static int Add2(int number1=30, int number2=20) // RETURN parametreli metotlarda metod içinde değer verilebilir
+//{
+
+//    return number1 + number2;
+
+
+//}
+// ÇARPMA METOT
+
+
+
+//Console.WriteLine(Multiply(2, 4));
+//Console.WriteLine(Multiply2(2, 4,5));
+//Console.WriteLine(Multiply(2, 4, 5,7));
+//Console.ReadLine();
+
+
+
+//static int Multiply(int number1, int number2)
+//{
+
+//    return number1 * number2;
+
+//}
+
+////  3 SAYI ÇARPMAK İSTERSEK 
+
+//static int Multiply2(int number1, int number2, int number3)
+//{
+
+//    return (number1 * number2 * number3);
+
+//}
+
+//static int Multiply3(int number1, int number2, int number3, int number4)
+//{
+
+//    return (number1 * number2 * number3*number4);
+
+//}  OVERLOADİNG
+
---
+++ rastgeleTektopveEnbyk.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace cders17
+{
+
+    class Program
+    {
+        static void metod1(int[] dizi)
+        {
+
+            int top = 0;
+            for (int x = 0; x < 10; x++)
+            {
+
+                if (dizi[x] % 2 == 1)
+                {
+                    top += dizi[x];
+                }
+
+            }
+
+            Console.WriteLine(".....................................................toplamları " + top);
+        }
+        static void metod2(int[] dizi)
+        {
+            Array.Sort(dizi);
+            Console.WriteLine(dizi[9]);
+
+
+
+
+        }
+
+        static void Main(string[] args)//random olarak girilen 10 elemanlı bir dizini içindeki tek olanların toplanmı 
+        {//metod1 de dizi elemanlarından en büyük olanı metod2  de bulan geriye değer dondürmeyen paremetre alan programı yazınız..
+            Random ras = new Random();
+            int[] dizi = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                dizi[i] = ras.Next(1, 20);
+                Console.WriteLine(dizi[i]);
+            }
+            metod1(dizi);
+            metod2(dizi);
+
+            Console.ReadLine();
+        }
+    }
+}
