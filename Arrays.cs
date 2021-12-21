233da317cfe2a86cff9089d73091ca855ede047a
Baktemur <75330735+Baktemur@users.noreply.github.com>
2021-12-21 20:07:31 +0300

    Create Arrays.cs

---
+++ Arrays.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string kredi1 = "Hızlı kredi:";
+            //string kredi2 = "Maaşını Halkbankk'tan Alanlara Özel";
+            //Console.WriteLine(kredi1);
+            //Console.WriteLine(kredi2);
+
+            //string[] krediler = new string[7];
+            //krediler[0] = "Hızlı Kredi";
+            //krediler[1] = " Kredi2";
+            //krediler[2] = " Kredi3";
+            //krediler[3] = " Kredi4";
+            //krediler[4] = " Kredi5";
+            //krediler[5] = " Kredi6";
+            //krediler[6] = " Kredi7";
+            //Console.WriteLine(krediler[0]);
+            string[] sayilar = new string[3];
+
+            sayilar[0] = "Küçük sayı";
+
+            sayilar[1] = "Ortanca Sayı";
+
+            sayilar[2] = "Büyük Sayı";
+
+            Console.WriteLine(sayilar[0]);
+
+            Console.WriteLine(sayilar[1]);
+
+            Console.WriteLine(sayilar[2]);
+
+
+        }
+    }
+}
