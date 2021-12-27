a2c5f5cae039669f574187ffabb3c63a6a53d37a
Baktemur <75330735+Baktemur@users.noreply.github.com>
2021-12-27 19:41:41 +0300

    Create DegerVeReferans.cs

---
+++ DegerVeReferans.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int sayi1 0 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+        }
+    }
+}
